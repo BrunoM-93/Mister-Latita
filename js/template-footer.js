ead540bff06469199e94d98669f36311c3762b2a
Bruno <bamoscardini@gmail.com>
2022-10-18 12:15:27 -0300

    Template-string para header y footer

---
+++ js/template-footer.js
@@ -0,0 +1,15 @@
+document.getElementById("footer").innerHTML = `
+<p>Copyright &copy; 2022 - Mister Latita</p>
+
+<!-- Contenedor de iconos con links a redes sociales -->
+<div class="redes-footer">
+    <ul>
+        <li><a class="facebook" href="https://www.facebook.com/elsrdelalatita" target="_blank"><i
+                    class="fa-brands fa-facebook"></i></a></li>
+        <li><a class="instagram" href="https://www.instagram.com/misterlatita/" target="_blank"><i
+                    class="fa-brands fa-instagram"></i></a></li>
+        <li><a class="whatsapp" href="https://api.whatsapp.com/send?phone=5493425317987" target="_blank"><i
+                    class="fa-brands fa-whatsapp"></i></a></li>
+    </ul>
+</div>
+`
---
+++ js/template-header.js
@@ -0,0 +1,29 @@
+document.getElementById("header").innerHTML = `
+<div><a href="index.html"><img class="logo" src="img/logo.png" alt="Mister Latita"></a></div>
+
+<div class="title-container">
+    <div class="title1">
+        <p>Mister Latita</p>
+    </div>
+    <div class="title2">
+        <p>Almac&eacute;n de Bebidas</p>
+    </div>
+</div>
+
+<div class="navbar-container">
+    <input type="checkbox" id="menu-check">
+    <label id="menu" for="menu-check">
+        <span id="menu-abrir">&#9776;</span>
+        <span id="menu-cerrar">X</span>
+    </label>
+    
+    <nav>
+        <ul>
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="encuentranos.html">Encuentranos</a></li>
+            <li><a href="contacto.html">Contacto</a></li>
+        </ul>
+    </nav>
+</div>
+`
