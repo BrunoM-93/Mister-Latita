7781d054ef1c2df909c91a08d5c03160e12533a9
Bruno <bamoscardini@gmail.com>
2022-10-26 15:52:50 -0300

    Mejoras en validaciones.js, valida y recarga pagina mostrando mensaje de confirmacion

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,57 +1,76 @@
-//Validaciones
+document.addEventListener('DOMContentLoaded', function () {
 
-(function(){
-    
-    var formulario = document.getElementById('form'),
-        boton = document.getElementById('boton');
+    //Validacion campos formulario
+    var formulario = document.getElementById('form');
 
-    var validarNombre = function(e){
-        if (formulario.nombre.value == 0){
+    var validarNombre = function (e) {
+        if (formulario.nombre.value == 0) {
             alert("Completá el Campo Nombre");
             e.preventDefault();
-            
-        }
+            return false;
+        };
+        return true;
     };
 
-    var validarCorreo = function(e){
-        if (formulario.correo.value == 0){
-            alert("Completá el campo Correo");
+    var validarCorreo = function (e) {
+        if (formulario.email.value == 0) {
+            alert("Completá el campo Correo Electrónico");
             e.preventDefault();
-            
-        }
+            return false;
+        };
+        return true;
     };
 
-    var validarTelefono = function(e){
-        if (formulario.telefono.value == ""){
+    var validarTelefono = function (e) {
+        if (formulario.telefono.value == "") {
             alert("Completá el campo Telefono");
             e.preventDefault();
-        }
-    }
-    
-    var validarMensaje = function(e){
-        if (formulario.mensaje.value == 0){
+            return false;
+        };
+        return true;
+    };
+
+    var validarMensaje = function (e) {
+        if (formulario.mensaje.value == 0) {
             alert("Completá el campo Mensaje");
             e.preventDefault();
-            
-        }
+            return false;
+        };
+        return true;
     };
 
-    var validar = function(e){
-        validarNombre(e);
-        validarCorreo(e);
-        validarTelefono(e);
-        validarMensaje(e);
-
+    var validar = function (e) {
+        return validarNombre(e) &&
+            validarCorreo(e) &&
+            validarTelefono(e) &&
+            validarMensaje(e);
     };
 
-    formulario.addEventListener("submit", validar);
-}
-())
+    formulario.addEventListener("submit", handleSubmit);
 
-//Resetea el Formulario cuando lo envia
 
-window.onbeforeunload = () => {
-    for(const form of document.getElementsByName('formulario')) {
-        form.reset();
-    }
-}
+    //Recarga formulario un vez validado y enviado, limpiando los campos.
+    async function handleSubmit(event) {
+        event.preventDefault();
+        if (validar()) {
+            const form = new FormData(this);
+            const response = await fetch(this.action, {
+                method: this.method,
+                body: form,
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
+            if (response.ok) {
+                this.reset();
+                alert('Gracias por contactarte con nosotros. En breve nos estaremos comunicando');
+            };
+        };
+    };
+});
+
+//Resetea el Formulario cuando vuelve a la pagina despues de enviado
+// window.onbeforeunload = () => {
+//     document.getElementById('form')
+//     form.reset();
+// }
